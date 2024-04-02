22e95daecad249ed9ab5f27d26ac1f2d190b34b3
Albaylar <furkandenizalbaylar@gmail.com>
2024-04-02 17:20:02 +0300

    Initial Commit

---
+++ University/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  University
+//
+//  Created by Furkan Deniz Albaylar on 2.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
